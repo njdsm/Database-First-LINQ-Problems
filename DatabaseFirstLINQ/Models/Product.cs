a6ccbbecadf3ffdac0d9b1b50421cf26f4e7fc5a
Aaron Kurowski <aaron12342@att.net>
2021-06-10 13:05:02 -0500

    Fixing the duplications

--- DatabaseFirstLINQ/Models/Product.cs
+++ DatabaseFirstLINQ/Models/Product.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System;
 using System.Collections.Generic;
 
@@ -21,27 +21,3 @@ namespace DatabaseFirstLINQ.Models
         public virtual ICollection<ShoppingCart> ShoppingCarts { get; set; }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-
-#nullable disable
-
-namespace DatabaseFirstLINQ.Models
-{
-    public partial class Product
-    {
-        public Product()
-        {
-            ShoppingCarts = new HashSet<ShoppingCart>();
-        }
-
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-
-        public virtual ICollection<ShoppingCart> ShoppingCarts { get; set; }
-    }
-}
->>>>>>> 82ff623d2165e987648cd7c0049f83d3dbbbb68b
--- DatabaseFirstLINQ/Models/ShoppingCart.cs
+++ DatabaseFirstLINQ/Models/ShoppingCart.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 #nullable disable
@@ -16,22 +15,3 @@ namespace DatabaseFirstLINQ.Models
         public virtual User User { get; set; }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-
-#nullable disable
-
-namespace DatabaseFirstLINQ.Models
-{
-    public partial class ShoppingCart
-    {
-        public int UserId { get; set; }
-        public int ProductId { get; set; }
-        public int? Quantity { get; set; }
-
-        public virtual Product Product { get; set; }
-        public virtual User User { get; set; }
-    }
-}
->>>>>>> 82ff623d2165e987648cd7c0049f83d3dbbbb68b
